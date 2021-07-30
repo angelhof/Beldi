361eacaafdd0e19ca43716b25aacb5308b5e39c1
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2021-07-30 16:50:07 +0300

    Read endpoints from file

--- scripts/singleop/run.sh
+++ scripts/singleop/run.sh
@@ -15,12 +15,24 @@ echo "Initializing Database"
 go run ./internal/singleop/init/init.go >/dev/null
 echo "Deploying"
 sls deploy -c singleop.yml
-read -p "Please Input HTTP gateway url for beldi-dev-bsingleop: " bp
-read -p "Please Input HTTP gateway url for beldi-dev-singleop: " p
-read -p "Please Input HTTP gateway url for beldi-dev-tsingleop: " tp
+
+## Get the endpoints from a file or ask them from the user and save them to the file if it doesnt exist
+endpoints_file="singleop.endpoints"
+
+if [ -f "$endpoints_file" ]; then
+  echo "Endpoints acquired from: $endpoints_file"
+  source "$endpoints_file"
+else
+  read -p "Please Input HTTP gateway url for beldi-dev-bsingleop: " bp
+  read -p "Please Input HTTP gateway url for beldi-dev-singleop: " p
+  read -p "Please Input HTTP gateway url for beldi-dev-tsingleop: " tp
+  echo "bp=$bp"
+  echo "p=$p"
+  echo "tp=$tp"
+fi
 
 ## TODO: Install wrk in submodules
-wrk_bin=../wrk/wrk
+wrk_bin=../wrk2/wrk
 # wrk_bin=./tools/wrk
 echo "Running baseline"
 ENDPOINT="$bp" "$wrk_bin" -t1 -c1 -d"$duration"s -R1 -s ./benchmark/singleop/workload.lua --timeout 10s "$bp" >/dev/null

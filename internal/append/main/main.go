e9516aa9661cd2cac0f39d3aec64dee469de0519
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2021-08-19 18:20:59 +0300

    Read and write the same item in all versions of append

--- internal/append/main/main.go
+++ internal/append/main/main.go
@@ -8,7 +8,6 @@ import (
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go/service/dynamodb/expression"
 	"github.com/eniac/Beldi/pkg/beldilib"
-	"github.com/lithammer/shortuuid"
 )
 
 var TXN = "DISABLE"
@@ -18,16 +17,33 @@ func SerializeSlice(slice []string) string {
 }
 
 func Handler(env *beldilib.Env) interface{} {
+	var slice []string = make([]string, 0)
+	len := 100
+	for i := 0; i < len; i++ {
+		slice = append(slice, "hi")
+	}
+
+	a := SerializeSlice(slice)
+	fmt.Printf("Serialized slice %s\n", a)
+
+	// ok, item := beldilib.TPLRead(env, data.Tflight(), flightId, []string{"V"})
+	// if !ok {
+	// 	return false
+	// }
+	// var flight Flight
+	// beldilib.CHECK(mapstructure.Decode(item["V"], &flight))
+	// if flight.Cap == 0 {
+	// 	return false
+	// }
+	// ok = beldilib.TPLWrite(env, data.Tflight(), flightId,
+	// 	aws.JSONValue{"V.Cap": flight.Cap})
+	// return ok
+
+	// Start a transaction to read the list, append to it, and then write it
+	// beldilib.BeginTxn(env)
+	// beldilib.TRead(env, "tappend", "K")
+
 	if TXN == "ENABLE" {
-		var slice []string = make([]string, 0)
-		len := 100
-		for i := 0; i < len; i++ {
-			slice = append(slice, "hi")
-		}
-
-		a := SerializeSlice(slice)
-		fmt.Printf("Serialized slice %s\n", a)
-
 		start := time.Now()
 		beldilib.TWrite(env, "tappend", "K", a)
 		fmt.Printf("DURATION DWrite %s\n", time.Since(start))
@@ -46,7 +62,6 @@ func Handler(env *beldilib.Env) interface{} {
 		return 0
 	}
 	if beldilib.TYPE == "BELDI" {
-		a := shortuuid.New()
 		start := time.Now()
 		beldilib.Write(env, "append", "K", map[expression.NameBuilder]expression.OperandBuilder{
 			expression.Name("V"): expression.Value(a),
@@ -72,7 +87,6 @@ func Handler(env *beldilib.Env) interface{} {
 		return 0
 	}
 	if beldilib.TYPE == "BASELINE" {
-		a := shortuuid.New()
 		start := time.Now()
 		beldilib.Write(env, "bappend", "K", map[expression.NameBuilder]expression.OperandBuilder{
 			expression.Name("V"): expression.Value(a),

3b791d06e9f697b1f4e23bb370e492d6c8a91c61
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2021-08-23 14:54:00 +0300

    Remove unnecessary init

--- internal/append/init/init.go
+++ internal/append/init/init.go
@@ -9,12 +9,7 @@ import (
 
 func ClearAll() {
 	beldilib.DeleteLambdaTables("append")
-	beldilib.DeleteLambdaTables("nop")
 	beldilib.DeleteTable("append-local")
-	// beldilib.DeleteTable("bappend")
-	// beldilib.DeleteTable("bnop")
-	// beldilib.DeleteLambdaTables("tappend")
-	// beldilib.DeleteLambdaTables("tnop")
 }
 
 func main() {
@@ -28,36 +23,14 @@ func main() {
 	ClearAll()
 	beldilib.WaitUntilAllDeleted([]string{
 		"append", "append-log", "append-collector",
-		"nop", "nop-log", "nop-collector",
 		"append-local", // For transactions
-		// "tappend", "tappend-log", "tappend-collector",
-		// "tnop", "tnop-log", "tnop-collector",
-		// "bappend", "bnop",
 	})
 	beldilib.CreateLambdaTables("append")
-	beldilib.CreateLambdaTables("nop")
 	beldilib.CreateMainTable("append-local")
 	beldilib.WaitUntilActive("append-local")
 
-	// beldilib.CreateBaselineTable("bappend")
-	// beldilib.CreateBaselineTable("bnop")
-
-	// beldilib.CreateTxnTables("tappend")
-	// beldilib.CreateTxnTables("tnop")
-
 	// TODO: Modify these to write a list
 	time.Sleep(60 * time.Second)
-	// beldilib.WriteNRows("append", "K", 20)
 
 	beldilib.Populate("append", "K", "", false)
-	// beldilib.Write(env, "bappend", "K", map[expression.NameBuilder]expression.OperandBuilder{
-	// 	expression.Name("V"): expression.Value(""),
-	// })
-	// beldilib.LibWrite("bappend", aws.JSONValue{"K": "K"}, map[expression.NameBuilder]expression.OperandBuilder{
-	// 	expression.Name("V"): expression.Value(1),
-	// })
-
-	// beldilib.LibWrite("tappend", aws.JSONValue{"K": "K"}, map[expression.NameBuilder]expression.OperandBuilder{
-	// 	expression.Name("V"): expression.Value(1),
-	// })
 }
